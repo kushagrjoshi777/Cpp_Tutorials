02cd5022db1f0891f3a1e838a9bf43112cd1f898
KukuCodes <77967013+kushagrjoshi777@users.noreply.github.com>
2025-04-16 19:08:07 +0530

    Add files via upload

---
+++ syntax/syntax1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello World"<<endl;
+    //You can add comments using double slash lmao
+    cout<<"Wow I really wrote this without chat gpt";
+    return 0;
+}
